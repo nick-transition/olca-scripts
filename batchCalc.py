1d6ba6c0bb0c2d4f525d1ce832ed774fc83ea31e
nick-transition <nstoddar@gmail.com>
2016-09-15 15:19:46 -0500

    Accumulating calculation values

--- batchCalc.py
+++ batchCalc.py
@@ -1,26 +1,53 @@
-system = olca.getSystem("rice")
+import org.openlca.core.results.ContributionResultProvider as rp
+import org.openlca.core.results.Contributions as contributions
+import csv
 
-
-
-reference = system.getReferenceProcess()
-location = reference.getLocation()
+# Find category folder
 category = olca.getCategory("Agriculture, forestry and fishing")
 subcat = category.getChildCategories().elementAt(2).name # log.info(elem.name) == Growing of rice
-
+system = olca.getSystem("rice")
 idX=[]
 
 def checkCat(p):
   if p.getCategory().name == subcat:
-    idX.append(p.getName())
+    idX.append(p.getId())
     return idX
   else:
     return idX
 
-#olca.inspect(olca)
 # Get list of target process ids
 olca.eachProcess(checkCat)
 
+header =['ParentProcess-Location-ChildProcess']
+# O(n^3)
+rows = {}
+for id in idX:
+# update rice product system with a process
+    replacement = olca.getProcess(id)
+    rName = replacement.name
+    rLoc = replacement.location.name
+    system.setReferenceProcess(replacement)
+    i=0
+    # Run calculation on the system
+    result = olca.analyze(system,olca.getMethod("TRACI"))
+    impacts = result.getImpactDescriptors()
+    processes = result.getProcessDescriptors()
+    for process in processes:
+    	rowValue=[]
+        for impact in impacts:
+            iName = impact.name+" ("+impact.getReferenceUnit()+")"
+            if i==0:
+                header.append(iName) if iName not in header else None
+            processContributions = result.getProcessContributions(impact)
+            rowValue.append(str(processContributions.getContribution(process).amount))
+    	rowId = rName+"-"+rLoc+"-"+process.name
+    	rows[rowId] = rowValue
+
+          #log.info("Name: {}, Amount: {}",process.name,str(processContributions.getContribution(process).amount))
+for key in rows.keys():
+    log.info("key:{} , vals:{}",key,str(rows[key]))
 
 
-for name in idX:
-  log.info(name)
+
+#f = open('C:/Users/nstoddar/Desktop/results_out.csv','wb')
+#writer = csv.writer(f)
